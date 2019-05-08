ce0e9d9ac90f8541fe4c70fb5cf2d7f9bf6e2a71
Ali <aliarasteh93@gmail.com>
2019-05-08 23:50:46 +0430

    add persian fonts, change to support API 17

--- folioreader/src/main/java/com/folioreader/ui/fragment/FolioPageFragment.kt
+++ folioreader/src/main/java/com/folioreader/ui/fragment/FolioPageFragment.kt
@@ -428,8 +428,8 @@ class FolioPageFragment : Fragment(),
                     mWebview!!.loadUrl(callHighlightSearchLocator)
 
                 } else if (isCurrentFragment) {
-                    val cfi = lastReadLocator!!.locations.cfi
-                    mWebview!!.loadUrl(String.format(getString(R.string.callScrollToCfi), cfi))
+                    val cfi = lastReadLocator?.locations?.cfi
+                    mWebview?.loadUrl(String.format(getString(R.string.callScrollToCfi), cfi))
 
                 } else {
                     if (spineIndex == mActivityCallback!!.currentChapterIndex - 1) {
